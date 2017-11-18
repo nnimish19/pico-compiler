e8ecee6ae4a4cc5568b00fdbaa7e07aff0dceaa3
nnimish19 <nnimish19@gmail.com>
2017-11-17 22:11:09 -0500

    appended JVM bytecode types to ASTNode Type

--- src/cop5556fa17/TypeUtils.java
+++ src/cop5556fa17/TypeUtils.java
@@ -3,45 +3,54 @@ package cop5556fa17;
 import cop5556fa17.Scanner.Token;
 
 public class TypeUtils {
-	
+
 	public static enum Type {
-		INTEGER,
-		BOOLEAN,
-		IMAGE,
-		URL,
-		FILE,
-		SCREEN,
-		NONE;
+		INTEGER("I"),
+		BOOLEAN("Z"),
+		IMAGE("Ljava/awt/image/BufferedImage;"),
+		URL("Ljava/net/URL;"),
+		FILE("Ljava/io/File;"),
+		SCREEN("Screen"),
+		NONE(null);
+
+		private final String name;
+
+	    private Type(String s) {
+	        name = s;
+	    }
+	    public String toString() {
+	        return this.name;
+	    }
 	}
 
 
 	public static Type getType(Token token){
 		switch (token.kind){
-		case KW_int: {return Type.INTEGER;} 
-		case KW_boolean: {return Type.BOOLEAN;} 
-		case KW_image: {return Type.IMAGE;} 
-		case KW_url: {return Type.URL;} 
+		case KW_int: {return Type.INTEGER;}
+		case KW_boolean: {return Type.BOOLEAN;}
+		case KW_image: {return Type.IMAGE;}
+		case KW_url: {return Type.URL;}
 		case KW_file: {return Type.FILE;}
 		case KW_SCREEN: {return Type.SCREEN;}
 			default :
-				break; 
+				break;
 		}
 		assert false;  //should not reach here
-		return null;  
+		return null;
 	}
-	
+
 	public static Type getType(Scanner.Kind k){
 		switch (k){
-		case KW_int: {return Type.INTEGER;} 
-		case KW_boolean: {return Type.BOOLEAN;} 
-		case KW_image: {return Type.IMAGE;} 
-		case KW_url: {return Type.URL;} 
+		case KW_int: {return Type.INTEGER;}
+		case KW_boolean: {return Type.BOOLEAN;}
+		case KW_image: {return Type.IMAGE;}
+		case KW_url: {return Type.URL;}
 		case KW_file: {return Type.FILE;}
 		case KW_SCREEN: {return Type.SCREEN;}
 			default :
-				break; 
+				break;
 		}
 		assert false;  //should not reach here
-		return null;  
+		return null;
 	}
 }
