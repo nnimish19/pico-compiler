a087fdaa04363639e1978cfac94bde484a9ffdb0
nnimish19 <nnimish19@gmail.com>
2017-12-03 01:48:18 -0500

    ImageResources.java: added image file path

--- src/cop5556fa17/ImageResources.java
+++ src/cop5556fa17/ImageResources.java
@@ -2,7 +2,7 @@ package cop5556fa17;
 
 public interface ImageResources {
 
-	public final static String imageFile1 = "";
+	public final static String imageFile1 = "/Users/nj/UFL/plp/eclipse_workspace/PLPHomework/src/cop5556fa17/ShelterPoint.jpg";
 	public final static String imageFile2 = "";
 
 	public final static String imageURL1 = "file://\\" + imageFile1;
